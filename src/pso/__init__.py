a429235ba77f8b5d2da36684e06ef9726d9832aa
ghadaSaeed <ghadasaeedmohammed@gmail.com>
2026-04-20 17:10:22 +0200

    Add files via upload

--- src/pso/__init__.py
+++ src/pso/__init__.py
@@ -1,9 +1,9 @@
 from .particle import Particle
-from .swarm import Swarm
-from .optimizer import PSOOptimizer
+from .fitness import FitnessEvaluator
+# from .swarm import Swarm
+# from .optimizer import PSOOptimizer
 
 __all__ = [
     "Particle",
-    "Swarm",
-    "PSOOptimizer"
+    "FitnessEvaluator"
 ]
---
+++ src/pso/particle.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class Particle:
+    def __init__(self, num_intersections, min_green=5, max_green=60):
+        
+        # التوقيتات الحالية (الحل)
+        self.position = np.random.uniform(min_green, max_green, num_intersections)
+        
+        # السرعة (هتتغير بيها التوقيتات)
+        self.velocity = np.random.uniform(-5, 5, num_intersections)
+        
+        # أحسن حل وصتله الجسيمة دي لوحدها
+        self.pbest_position = self.position.copy()
+        self.pbest_score = float('inf')
+
+def update_velocity(self, gbest_position, inertia=0.5, c1=1.5, c2=1.5):
+    r1 = np.random.random(len(self.position))
+    r2 = np.random.random(len(self.position))
+    
+    self.velocity = (inertia * self.velocity +
+                     c1 * r1 * (self.pbest_position - self.position) +
+                     c2 * r2 * (gbest_position - self.position))
+
+def update_position(self, min_green=5, max_green=60):
+    self.position = self.position + self.velocity
+    self.position = np.clip(self.position, min_green, max_green)
